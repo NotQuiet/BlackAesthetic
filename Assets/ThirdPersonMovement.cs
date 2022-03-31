738d2404997f2c65d7211d28ff1704e488a0ab4a
NotQuiet <mikhail.work.flow@gmail.com>
2022-03-31 13:53:38 +0300

    Add movement smoothing

--- Assets/ThirdPersonMovement.cs
+++ Assets/ThirdPersonMovement.cs
@@ -6,7 +6,12 @@ public class ThirdPersonMovement : MonoBehaviour
 {
     public CharacterController controller;
 
+    public Transform cam;
+
     public float speed = 6f;
+
+    public float turnSmoothTime = 0.1f;
+    private float turnSmoothVelocity;
 
     // Update is called once per frame
     void Update()
@@ -18,11 +23,12 @@ public class ThirdPersonMovement : MonoBehaviour
 
         if(direction.magnitude >= 0.1f)
         {
-            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
 
-            transform.rotation = Quaternion.Euler(0f, targetAngle, 0f);
-
-            controller.Move(direction * speed * Time.deltaTime);
+            Vector3 moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            controller.Move(moveDirection.normalized * speed * Time.deltaTime);
         }
     }
 }

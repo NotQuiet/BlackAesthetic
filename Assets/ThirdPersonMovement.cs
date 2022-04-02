295da9bbf2dfb468faf58edf4cff6fc8a056694f
NotQuiet <mikhail.work.flow@gmail.com>
2022-04-02 21:33:33 +0300

    Add namespace for Eli

--- Assets/ThirdPersonMovement.cs
+++ Assets/ThirdPersonMovement.cs
@@ -2,33 +2,36 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ThirdPersonMovement : MonoBehaviour
+namespace Eli
 {
-    public CharacterController controller;
-
-    public Transform cam;
-
-    public float speed = 6f;
-
-    public float turnSmoothTime = 0.1f;
-    private float turnSmoothVelocity;
-
-    // Update is called once per frame
-    void Update()
+    public class ThirdPersonMovement : MonoBehaviour
     {
-        float horizontal = Input.GetAxisRaw("Horizontal");
-        float vertical = Input.GetAxisRaw("Vertical");
+        public CharacterController controller;
 
-        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+        public Transform cam;
 
-        if(direction.magnitude >= 0.1f)
+        public float speed = 6f;
+
+        public float turnSmoothTime = 0.1f;
+        private float turnSmoothVelocity;
+
+        // Update is called once per frame
+        void Update()
         {
-            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
-            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
-            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+            float horizontal = Input.GetAxisRaw("Horizontal");
+            float vertical = Input.GetAxisRaw("Vertical");
 
-            Vector3 moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
-            controller.Move(moveDirection.normalized * speed * Time.deltaTime);
+            Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+            if (direction.magnitude >= 0.1f)
+            {
+                float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+                float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+                transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+                Vector3 moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+                controller.Move(moveDirection.normalized * speed * Time.deltaTime);
+            }
         }
     }
 }

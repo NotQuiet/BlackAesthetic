f363fc45166986bebccdf398388d0fb55adbbf5c
NotQuiet <mikhail.work.flow@gmail.com>
2022-04-15 17:03:24 +0300

    Unloading scene method added

--- Assets/Scripts/Controllers/StateController.cs
+++ Assets/Scripts/Controllers/StateController.cs
@@ -11,6 +11,9 @@ namespace Eli
 {
     public class StateController
     {
+        private string _currentScene = "GameMenu";
+
+        public string CurrentScene { get { return _currentScene; } private set { _currentScene = value; } }
         public void ChangeState(GameState state)
         {
             switch (state)
@@ -29,8 +32,18 @@ namespace Eli
 
         private async Task LoadScene(string sceneName)
         {
-            var asuncOperation = Addressables.LoadSceneAsync(sceneName, LoadSceneMode.Additive);
-            await asuncOperation.Task;
+            var asyncOperation = Addressables.LoadSceneAsync(sceneName, LoadSceneMode.Additive);
+            await asyncOperation.Task;   
+
+            UnloadScene(CurrentScene);
+
+            CurrentScene = sceneName;
+        }
+
+
+        private async Task UnloadScene(string sceneName)
+        {
+            SceneManager.UnloadSceneAsync(sceneName);
         }
     }
 }

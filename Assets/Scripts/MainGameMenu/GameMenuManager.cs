9d8874ff3539b39cf90642de81653ca9b37debcc
NotQuiet <mikhail.work.flow@gmail.com>
2022-04-19 11:38:02 +0300

    Add settings/exit windows
    And add some UI animations

--- Assets/Scripts/MainGameMenu/GameMenuManager.cs
+++ Assets/Scripts/MainGameMenu/GameMenuManager.cs
@@ -8,6 +8,11 @@ namespace Eli
     public class GameMenuManager : MonoBehaviour, IRequireController<StateController>, IRequireController<GameManagerController>
     {
         [SerializeField] private Button _startGame;
+        [SerializeField] private Button _settingsButton;
+        [SerializeField] private Button _exitGameButton;
+
+        [SerializeField] private CanvasGroup _settingsWindow;
+        [SerializeField] private CanvasGroup _exitConfirmationWindow;
 
         private GameManagerController _gameManagerController;
         private StateController _stateController;
@@ -16,6 +21,38 @@ namespace Eli
         private void OnEnable()
         {
             _startGame.onClick.AddListener(OnStartGameClick);
+            _settingsButton.onClick.AddListener(OnSettingsButtonClick);
+            _exitGameButton.onClick.AddListener(OnExitButtonClick);
+
+            SetLocalScalesOnPanels();
+        }
+
+        private void SetLocalScalesOnPanels()
+        {
+            _settingsWindow.transform.localScale = Vector2.zero;
+            _exitConfirmationWindow.transform.localScale = Vector2.zero;
+        }
+
+        private void OnSettingsButtonClick()
+        {
+            Open(_settingsWindow, 0.3f);
+        }
+
+        private void OnExitButtonClick()
+        {
+            Open(_exitConfirmationWindow, 0.3f);
+        }
+
+        private void Open(CanvasGroup panel, float time)
+        {
+            panel.gameObject.SetActive(true);
+            panel.transform.LeanScale(Vector2.one, time).setEaseInBack();
+        }
+
+        private void Close(CanvasGroup panel, float time)
+        {
+            panel.transform.LeanScale(Vector2.zero, time).setEaseInBack();
+            panel.gameObject.SetActive(false);
         }
 
         private void OnStartGameClick()

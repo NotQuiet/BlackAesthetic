7291b9d0d1a99f456d164e5c6f6b20063d0b1afe
NotQuiet <mikhail.work.flow@gmail.com>
2022-04-21 20:58:21 +0300

    Update Player.cs

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -4,5 +4,15 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    
+    [SerializeField] private InputMaster controls;
+
+    private void Awake()
+    {
+        controls.Player.Jump.performed += contex => OnJump();
+    }
+
+    private void OnJump()
+    {
+        Debug.Log("Jump");
+    }
 }
